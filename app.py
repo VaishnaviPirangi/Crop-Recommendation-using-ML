3fc191d46e28462ce6368eaec908dea0e5f09c2f
Vaishnavi Pirangi <vaishnavipirangi007@gmail.com>
2025-11-15 00:50:28 +0530

    Fix import statements and data loading in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 model=pkl.load(open("Crop_recommendation.pkl","rb"))
 
 st.header=('Crop Recommendation')
-data=pd.read_csv(r"C:\Users\VAISHNAVI\OneDrive\Desktop\Crop Recommendation Project\crop_dataset.csv")
+data=pd.read_csv("crop_dataset.csv")
 
 N=st.number_input('Enter Nitrogen (mg/kg)')
 P=st.number_input('Enter Phosphorous (mg/kg)')
@@ -22,4 +22,5 @@ if st.button("Recommend Crop"):
     output=model.predict(input)
     out_str='The recommended crop is '+str(output[0])
     st.success(out_str)
+
 
